b87a444f9e466bd9533b33444887bb480f142816
edcan-dev <israel.da.queenie@gmail.com>
2023-04-30 11:36:06 -0600

    30/04/23 - Primer despligue

--- src/main/java/com/edic/azureadtest/controllers/AppController.java
+++ src/main/java/com/edic/azureadtest/controllers/AppController.java
@@ -1,15 +1,32 @@
 package com.edic.azureadtest.controllers;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import javax.print.attribute.standard.Media;
 
 
 @Controller
 public class AppController {
 
     @RequestMapping("/exam/home")
-    public String helloWorld(Model model) {
+    public String helloWorl(Model model) {
         return "screen_03";
     }
+
+    @GetMapping(value = "/exam/end")
+    public String perritoAsu(Model model) {
+        //System.out.println(body);
+        return "screen_04";
+    }
+
+    @PostMapping(value = "/exam/end", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public String helloWorld(@RequestBody String body) {
+        System.out.println(body);
+        return "ok";
+    }
+
+
 }
--- src/main/java/com/edic/azureadtest/controllers/AuthController.java
+++ src/main/java/com/edic/azureadtest/controllers/AuthController.java
@@ -2,6 +2,7 @@ package com.edic.azureadtest.controllers;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.security.access.prepost.PreAuthorize;
 
