24b8e2f4e8968fc8ff098bf855b81d65056f6967
edcan-dev <israel.da.queenie@gmail.com>
2023-04-30 23:53:58 -0600

    30/04/23 -Segundo despligue con target

--- src/main/java/com/edic/azureadtest/controllers/AppController.java
+++ src/main/java/com/edic/azureadtest/controllers/AppController.java
@@ -1,6 +1,11 @@
 package com.edic.azureadtest.controllers;
 
 import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -12,17 +17,18 @@ import javax.print.attribute.standard.Media;
 public class AppController {
 
     @RequestMapping("/exam/home")
-    public String helloWorl(Model model) {
+    public String helloWorl(Model model, Authentication authentication) {
+        // System.out.println(authentication.getPrincipal());
+        DefaultOidcUser user = (DefaultOidcUser) authentication.getPrincipal();
+        System.out.println("Se ha loggeado: " + user.getPreferredUsername());
+
+        model.addAttribute("username",user.getPreferredUsername());
+        model.addAttribute("name",authentication.getName());
         return "screen_03";
     }
 
-    @GetMapping(value = "/exam/end")
-    public String perritoAsu(Model model) {
-        //System.out.println(body);
-        return "screen_04";
-    }
-
     @PostMapping(value = "/exam/end", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PreAuthorize("isAuthenticated()")
     public String helloWorld(@RequestBody String body) {
         System.out.println(body);
         return "ok";
--- src/main/java/com/edic/azureadtest/controllers/AuthController.java
+++ src/main/java/com/edic/azureadtest/controllers/AuthController.java
@@ -1,6 +1,14 @@
 package com.edic.azureadtest.controllers;
+import com.google.gson.JsonObject;
+import com.microsoft.graph.authentication.TokenCredentialAuthProvider;
+import com.microsoft.graph.models.User;
+import com.microsoft.graph.requests.GraphServiceClient;
+import com.microsoft.graph.requests.UserRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -8,6 +16,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Objects;
 
 @Controller
 @RequestMapping("/auth")
@@ -24,7 +33,15 @@ public class AuthController {
     @GetMapping("/azuread")
     @ResponseBody
     @PreAuthorize("isAuthenticated()")
-    public void auth(HttpServletResponse response) throws IOException {
+    public void auth(HttpServletResponse response,
+                     @RegisteredOAuth2AuthorizedClient("graph") OAuth2AuthorizedClient graphClient, Authentication authentication
+    ) throws IOException {
+
+        //System.out.println(graphClient.getAccessToken().getTokenValue());
+        System.out.println(authentication.getPrincipal());
+
+
+
         response.sendRedirect("/exam/home");
     }
 }
